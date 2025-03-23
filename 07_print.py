9ccf301a218df3bf7c0746fecae6a4666f9f6135
Boris Kolda <boris.kolda@gmail.com>
2025-03-23 17:57:41 +0100

    07 print

---
+++ 07_print.py
@@ -0,0 +1,4 @@
+print("Naučili jsme se mnoho věcí o stringu")
+print('Spojení stringu děláme pomocí znaménka "+"')
+print("Také jsme používali print('nějaký text')")
+print("První řádek\nDruhý řádek")
